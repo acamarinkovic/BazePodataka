486b725dc3432c1c902e05c98ad1a0f154b3c27d
acamarinkovic <acamarinkovic93@gmail.com>
2019-06-07 11:21:02 +0200

    Update Form1.cs

--- SistemiZaUpravljanjeBazamaPodatka/Form1.cs
+++ SistemiZaUpravljanjeBazamaPodatka/Form1.cs
@@ -23,29 +23,20 @@ namespace SistemiZaUpravljanjeBazamaPodatka
             string connetionString = null;
             SqlConnection cnn;
 
-            //koriscenjem SqlConnectionStringBuilder validiramo connection string (sprecavano unos SQL Injection pri konekciji). Takodje mozemo lako menjati vrednost connection stringa
+           
             SqlConnectionStringBuilder builder =
                 new SqlConnectionStringBuilder(GetConnectionStringWindows());
 
             Console.WriteLine("Windows connection string:");
             Console.WriteLine(builder.ConnectionString);
-
-            //SQL Server login nije siguran jer podaci idu preko mreze, cime login moze da bude narusen.
-            //Console.WriteLine("SQL Server connection string:");
             //builder.ConnectionString = GetConnectionStringSQLServerLogin();
-
-            builder["initial catalog"] = "SZUBP;Server=192.168.1.2";
-            //Ovde klijent trazi bazu podataka pod imenom "AdventureWorks2014;Server=192.168.1.2" za razliku od prethodnog resenja gde klijent gadja drugi server
+            builder["initial catalog"] = "SZUBP;Server=192.168.4.1"
             connetionString = builder.ConnectionString;
-
             cnn = new SqlConnection(connetionString);
-
-
             try
             {
                 cnn.Open();
                 MessageBox.Show("Connection Open");
-
                 cnn.Close();
             }
             catch (Exception ex)
@@ -71,29 +62,20 @@ namespace SistemiZaUpravljanjeBazamaPodatka
             SqlConnection cnn;
             SqlCommand command;
             string parametar;
-
-
             connetionString = "Server=(local);Integrated Security=SSPI;Initial Catalog=SZUBP";
-
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connetionString);
-
             connetionString = builder.ConnectionString;
-
             cnn = new SqlConnection(connetionString);
-
-
             try
             {
                 cnn.Open();
                 Console.WriteLine("Connection Open");
-
                 parametar = "'Acimir'";
                 command = new SqlCommand("SELECT * FROM [dbo].[User] WHERE FirstName = " + parametar + ";", cnn);
                 var reader = command.ExecuteReader();
                 reader.Read();
                 MessageBox.Show((string)reader["FirstName"]);
                 reader.Close();
-                //po izvrsenju programa, sql server nece da izvrsi dodatnu sql naredbu
                 parametar = "'Acimir';INSERT INTO  [User] (FirstName, Location) VALUES ('Zika', 'ObalaSlonovace');--";
                 command = new SqlCommand("SELECT * FROM [User] WHERE FirstName = @parameter;", cnn);
                 command.Parameters.AddWithValue("parameter", parametar);
@@ -117,9 +99,6 @@ namespace SistemiZaUpravljanjeBazamaPodatka
             string database = "SZUBP;Server=192.168.1.2";
             connetionString = "Server=(local);Integrated Security=SSPI;Initial Catalog=" + database + "; ";
             cnn = new SqlConnection(connetionString);
-            //Ovde klijent trazi drugi server sa imenom (IP adresom) "192.168.1.2" za razliku od resenja koje koristi SqlConnectionStringBuilder
-
-
             try
             {
                 cnn.Open();
@@ -141,14 +120,9 @@ namespace SistemiZaUpravljanjeBazamaPodatka
             SqlCommand command;
             SqlDataReader reader;
             string parametar;
-
-
             connetionString = "Server=(local);Integrated Security=SSPI;Initial Catalog=SZUBP";
-
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connetionString);
-
             connetionString = builder.ConnectionString;
-
             cnn = new SqlConnection(connetionString);
 
             try
@@ -161,7 +135,7 @@ namespace SistemiZaUpravljanjeBazamaPodatka
                 reader = command.ExecuteReader();
                 MessageBox.Show(reader.FieldCount.ToString());
                 reader.Close();
-                //po izvrsenju programa, sql server ce da izvrsi obe sql naredbe
+         
                 parametar = "'Acimir';DROP TABLE [Table_1];--";
                 command = new SqlCommand("SELECT * FROM [dbo].[User] WHERE FirstName = " + parametar + ";", cnn);
                 reader = command.ExecuteReader();
